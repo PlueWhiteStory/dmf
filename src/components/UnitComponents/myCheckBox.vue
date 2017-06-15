861685979e0eaf6eb915fae21fab88260e05caee
Irene <1262518173@qq.com>
2017-06-15 17:30:10 +0800

    'msg'

--- src/components/UnitComponents/myCheckBox.vue
+++ src/components/UnitComponents/myCheckBox.vue
@@ -1,14 +1,23 @@
 <template class="myCheckBox">
-  <input type="checkbox" :value="value" v-model="selected" @on-change="onchange"></input>
+  <span class="ivu-checkbox,{ivu-checkbox-checked:true}">
+    <span class="ivu-checkbox-inner">
+  <input type="checkbox" :value="value" v-model="selected" class="ivu-checkbox-input" @on-change="onchange"/>
+    </span>
+    </span>
 </template>
 <script>
+  import 'iview/dist/styles/iview.css';
     export default{
         name: "myCheckBox",
         data(){
             return {
+
             }
         },
       computed:{
+            status{
+              //const checked = event.target.checked;
+            },
             selected(){
                 if(this.ctype=="anachnos")
                 return this.store.state.anachnos;
